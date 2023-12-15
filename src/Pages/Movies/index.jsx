435f1d976f2289e532c014119d44492a09c823f3
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-12-15 15:53:42 -0500

    Add basic css

--- src/Pages/Movies/index.jsx
+++ src/Pages/Movies/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import  { Link } from "react-router-dom"
+import  "./Movies.css"
 
 import { getMovies } from "../../utilities/movies-service"
 
--- src/Pages/Show/index.jsx
+++ src/Pages/Show/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./Show.css";
 
 import { showMovie } from "../../utilities/movies-service";
 
