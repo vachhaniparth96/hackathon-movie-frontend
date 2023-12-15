c82a320583d1c42e054c5a7865b252dee7d038bd
Parth Vachhani <vachhani.parth.96@gmail.com>
2023-12-15 09:58:26 -0500

    further stubbing of front-end skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
-import { useState } from 'react'
 import Header from './Components/Header'
 import Main from './Components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
